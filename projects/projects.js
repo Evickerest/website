5b0277623a00cefab71dece39c9932cc4f623b01
Evickerst <ejmoras17@gmail.com>
2024-02-09 13:47:47 -0500

    changed layout of projects

---
+++ projects/projects.js
@@ -0,0 +1,13 @@
+// cardSections = document.querySelectorAll(".card-section");
+
+// cardSections.forEach( cardSection => {
+    
+//     cardSection.addEventListener( "click", () => {
+//         subCardContent = cardSection.querySelector(".sub-card-section");
+//         if( subCardContent.hasAttribute("show") ) {
+//             subCardContent.removeAttribute("show");
+//         } else {
+//             subCardContent.setAttribute("show","");
+//         }
+//     });
+// });
